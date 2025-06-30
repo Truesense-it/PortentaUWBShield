24061077d7d6570b8e596fdfa650aab7d1ad8e42
Truesense-it <pierpaolo.lento@truesense.it>
2025-06-30 14:26:16 +0200

    update TdoA anchor class

--- src/uwbapps/UWBUltdoaAnchor.hpp
+++ src/uwbapps/UWBUltdoaAnchor.hpp
@@ -11,20 +11,21 @@
 
 class UWBUltdoaAnchor : public UWBSession {
 public:     
-    UWBUltdoaAnchor(uint32_t session_ID, UWBMacAddress srcAddr) 
+    UWBUltdoaAnchor(uint32_t session_ID, UWBMacAddress srcAddr, uint32_t duration=1200) 
     {
         sessionID(session_ID);
         sessionType(uwb::SessionType::RANGING);
         rangingParams.deviceRole(uwb::DeviceRole::UT_ANCHOR);
         rangingParams.deviceType(uwb::DeviceType::CONTROLEE);
-        rangingParams.multiNodeMode(uwb::MultiNodeMode::ONE_TO_MANY);
+        rangingParams.multiNodeMode(uwb::MultiNodeMode::MULTICAST);
         rangingParams.rangingRoundUsage(uwb::RangingMethod::TDOA); 
         rangingParams.scheduledMode(uwb::ScheduledMode::TIME_SCHEDULED);
         rangingParams.deviceMacAddr(srcAddr);
+        
 
         appParams.frameConfig(uwb::RfFrameConfig::Sfd_Sts);
         appParams.stsConfig(uwb::StsConfig::StaticSts);
-        //appParams.rangingDuration(1200);
+        appParams.rangingDuration(duration);
         appParams.uplinkTdoaTimestamp(2); 
         appParams.addOrUpdateParam(buildScalar(uwb::AppConfigId::SessionInfoNtf, 1));
         appParams.sfdId(0);
